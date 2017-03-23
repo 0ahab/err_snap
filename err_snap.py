9dfb8102599c170882c8ef2b937bf2c445939e62
Eric Kloster <ekloster@akamai.com>
2017-03-23 08:37:34 -0400

    Fix command name for subprocess.

--- err_snap.py
+++ err_snap.py
@@ -7,7 +7,7 @@ class GetSnap(BotPlugin):
 	@botcmd
 	def getsnap(self, msg, args):
 		get_snap_cmd = 'imagesnap ~/$(date +%y%m%d%H%M%S).png'
-		get_snap, err = subprocess.Popen(get_current_sprint_cmd, shell=True, stdout=subprocess.PIPE).communicate()
+		get_snap, err = subprocess.Popen(get_snap_cmd, shell=True, stdout=subprocess.PIPE).communicate()
 		file_line = get_snap.splitlines()[-1]
 
 		return(file_line)
