6dd6ebba6323cde0c613acfab47b991342d38ebf
Eric Kloster <ekloster@akamai.com>
2017-03-24 16:28:16 -0400

    Fixing upload.

--- getsnap.py
+++ getsnap.py
@@ -6,13 +6,16 @@ class GetSnap(BotPlugin):
 	"""Errbot plugin to fetch current sprint details"""
 	@botcmd
 	def getsnap(self, msg, args):
-		# get_snap_cmd = 'imagesnap ~/snap$(date +%y%m%d%H%M%S).png'
-		# get_snap, err = subprocess.Popen(get_snap_cmd, shell=True, stdout=subprocess.PIPE).communicate()
-		# file_line = get_snap.splitlines()[-1]
-		# filename = file_line.split('/')[-1]
+		get_snap_cmd = 'imagesnap ~/snap$(date +%y%m%d%H%M%S).png'
+		get_snap, err = subprocess.Popen(get_snap_cmd, shell=True, stdout=subprocess.PIPE).communicate()
+		file_line = get_snap.splitlines()[-1]
+		filename = file_line.split('/')[-1]
 		get_key_name = 'ls ~/.ssh/ | grep -i digital | grep -vi pub'
 		key_name, err = subprocess.Popen(get_key_name, shell=True, stdout=subprocess.PIPE).communicate()
-		return(key_name)
-		# up_snap_cmd = 'scp /Users/eric/' + filename + ' -i .ssh/foo reverse@`host appdev.0ahab.net ns3.digitalocean.com | grep has.address | awk \'{print $4}\'`:./'
 
+		key_name = key_name.replace('\n', '')
+
+		up_snap_cmd = 'scp ~/' + filename + ' -i ~/.ssh/'+key_name +' reverse@`host appdev.0ahab.net ns3.digitalocean.com | grep has.address | awk \'{print $4}\'`:./'
+
+		return(filename + " uploaded.")
 		# return(user_dir)
