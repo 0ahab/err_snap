4a82d97050d71f24bd74d7f5c0f3aa3156a17195
Eric Kloster <ekloster@akamai.com>
2017-03-24 16:08:51 -0400

    File cleanup.

--- getsnap.py
+++ getsnap.py
@@ -6,7 +6,7 @@ class GetSnap(BotPlugin):
 	"""Errbot plugin to fetch current sprint details"""
 	@botcmd
 	def getsnap(self, msg, args):
-		get_snap_cmd = 'imagesnap ~/$(date +%y%m%d%H%M%S).png'
+		get_snap_cmd = 'imagesnap ~/snap$(date +%y%m%d%H%M%S).png'
 		get_snap, err = subprocess.Popen(get_snap_cmd, shell=True, stdout=subprocess.PIPE).communicate()
 		file_line = get_snap.splitlines()[-1]
 
